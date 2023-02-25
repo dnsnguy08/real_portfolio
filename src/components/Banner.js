ecf97e777704bdc3630e036204c6a02addbe2f97
Dennis Nguyen <dnsnguy08@gmail.com>
2023-02-24 17:07:50 -0800

    styling fixes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -58,15 +58,18 @@ export const Banner = () => {
                 <div className="wrap"> {text}</div>
                 <div>Engineer</div>
               </h1>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
+              <div id="banner-bx">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
+              </div>
               <HashLink to="#connect">
                 <button id="connectBtn" onClick={() => console.log("connect")}>
                   Let's Connect <ArrowRightCircle size={25} />
